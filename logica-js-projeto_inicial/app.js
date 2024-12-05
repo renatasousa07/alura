8ca890c87cca801391e1049830469c86f8b49752
renatasousa07 <renatavsousa07@gmail.com>
2024-12-05 16:52:06 -0300

    template strings

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert('Boas-vindas ao jogo do Número Secreto');
-let numeroSecreto = 5;
+let numeroSecreto = 8;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// Se o chute for igual ao número secreto 
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto}`);
+} else {
+    alert('Você Errou :(');
 }
