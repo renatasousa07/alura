686a13642ab078e461f215b526fd6f7dd3e89f99
renatasousa07 <renatavsousa07@gmail.com>
2024-12-05 13:22:04 -0300

    final

--- desafio-js-projeto_inicial/app.js
+++ desafio-js-projeto_inicial/app.js
@@ -1,2 +1,13 @@
 alert('Boas Vindas ao Nosso Site');
-let 
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+alert('Erro! Preencha todos os campos');
+let mensagemDeErro = 'Erro! preencha todos os campos';
+alert(mensagemDeErro);
+let nomes = prompt('Qual o Seu nome?');
+let idades = prompt('Digite sua idade');
+if (idades >= 18){
+    alert('Pode tirar a habilitação!');
+}
